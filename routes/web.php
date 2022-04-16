23c70cd5acdbbe443006b7f1ec58371a46ff7abf
is41-grigorevps <pavel51393@gmail.com>
2022-04-16 13:54:00 +0500

    Задание от 16.04.2022

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,69 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//1
+Route::get("my-name",function (){
+    return "Григорьев Павел Сергеевич";
+});
+//2
+Route::get('my-friend',function (){
+   return "Белый Семен Андреевич";
+});
+//3
+Route::get('get-friend/{name}',function ($name){
+    return $name;
+});
+//4
+Route::get('my-city/{city}',function ($city){
+    return $city;
+})->where('city','[A-za-z]+');
+
+//5
+Route::get('level/{lvl}',function ($lvl){
+    if($lvl>=0&&$lvl<=25){
+        return "новичок";
+    }
+    else if ($lvl>=26&&$lvl<=50){
+        return "специалист";
+    }
+    else if ($lvl>=51&&$lvl<=75){
+        return "босс";
+    }
+    else if ($lvl>=76&&$lvl<=98){
+        return "старик";
+    }
+    else if ($lvl==99){
+        return "король";
+    }
+});
+//6
+Route::get('working/{name}/{date}',function ($name,$date){
+
+    return $name . " " . $date;
+
+})->where(['name'=>'[A-za-z]+','date '=>'[0-9]']);
+//7
+Route::get('power',function (){
+    return route('power');
+})->name('power');
+//8
+Route::prefix('admin')->group(function (){
+    Route::get('login',function (){
+        return route('login');
+    })->name('login');
+    Route::get('logout',function (){
+        return route('logout');
+    })->name('logout');
+    Route::get('info',function (){
+        return route('info');
+    })->name('info');
+    Route::get('color',function (){
+        return route('color');
+    })->name('color');
+});
+//9
+Route::redirect('admin/web','color');
+//10
+Route::get('color/{hex}',function ($hex){
+   return "<div style='background-color: $hex; width:150px ; height:150px;border: solid red 1px'></div> Color-$hex";
+})->where('hex','[A-Fa-f0-9]{6}');
